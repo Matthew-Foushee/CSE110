beb89cb346a67aefb8ca59be7d8525648628a13d
Matthew-Foushee <mfoushee@byui.edu>
2025-05-19 10:42:13 -0700

    Create Prepare05.py

---
+++ Prepare05.py
@@ -0,0 +1,25 @@
+numbers = input("Give me two numbers: ").split()
+num1 = int(numbers[0])
+num2 = int(numbers[1])
+
+if(num1 > num2):
+  print("The first number is greater")
+else:
+  print("The first number is not greater")
+
+if(num1 == num2):
+  print("the numbers are equal")
+else:
+  print("The numbers are not equal")
+if(num2 > num1):
+  print("the second number is greater")
+else:
+  print("The second number is not greater")
+
+animal = input("What is your favorite animal")
+
+if(animal == "wolf"):
+  print("That is my favorite animal too!!")
+else:
+  print("That is not my favorite animal :(")
+
